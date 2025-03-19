65893abbfcc38b5c99b6fccf0d17916426179685
WhaleFall <166845494@qq.com>
2025-03-19 11:47:04 +0800

    timer intr pass to S mode

--- kernel/trap/trap.c
+++ kernel/trap/trap.c
@@ -2,9 +2,18 @@
 #include <memlayout.h>
 #include <debug.h>
 
+extern char kernelvec[];
+
+#define PANIC(type) do { \
+    log("unexpected " #type " irq: %d", irq); \
+    log("scause: %p", (void*)scause); \
+    log("sepc: %p", (void*)sepc); \
+    log("sstatus: %p", (void*)sstatus); \
+    panic("kernel_trap"); \
+} while(0)
 
 
-void __attribute__((aligned(4)))
+void
 kernel_trap()
 {
     log("kernel trap");
@@ -12,27 +21,27 @@ kernel_trap()
     uint64 sepc = r_sepc();
     uint64 sstatus = r_sstatus();
 
-    Assert((sstatus & SSTATUS_SPP) == 0, "kerneltrap: not from supervisor mode");
-    Assert((sstatus & SSTATUS_SIE) != 0, "kerneltrap: sie must be 1");
-
-    if (scause == 0x8000000000000005L) {
-        // timer interrupt
-        timervec();
-        w_sip(r_sip() & ~2);
-        log("receive timer interrupt");
+    if (scause & (1UL << 63)) {
+        // interrupt, not exception
+        int irq = scause & 0xfff;
+        if (irq == 0x1) {
+            log("receive timer interrupt");
+            // clear timer interrupt
+            w_sip(r_sip() & ~2);
+        }
+        else {
+            PANIC(interrupt);
+        }
+    } else {
+        // exception
+        
     }
-    else {
-        log("unexpected scause %p", (void*)scause);
-        log("sepc=%p", (void*)sepc);
-        panic("kerneltrap");
-    }
-
 }
 
 
 void
 trap_init_hart()
 {
-    w_stvec((uint64)kernel_trap);
+    w_stvec((uint64)kernelvec);
     log("trap init hart");
 }
