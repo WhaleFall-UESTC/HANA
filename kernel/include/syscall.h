31433dd63b1c40f5aaaa39facc5caee202ce4265
WhaleFall <166845494@qq.com>
2025-04-22 17:15:04 +0800

    oscomp syscall

--- kernel/include/syscall.h
+++ kernel/include/syscall.h
@@ -3,58 +3,46 @@
 
 #include <common.h>
 
-enum
-{
-    SYS_BLANK,
+/* oscmop requires */
 
-    // File Operations
-    SYS_open,
-    SYS_read,
-    SYS_write,
-    SYS_close,
-    SYS_lseek,
-    SYS_stat,
-    SYS_fstat,
-    SYS_unlink,
-    SYS_mkdir,
-    SYS_rmdir,
-    SYS_link,
-    SYS_symlink,
-    SYS_access,
+// FIle System
+#define SYS_getcwd 17
+#define SYS_pipe2 59
+#define SYS_dup 23
+#define SYS_dup3 24
+#define SYS_chdir 49
+#define SYS_openat 56
+#define SYS_close 57
+#define SYS_getdents64 61
+#define SYS_read 63
+#define SYS_write 64
+#define SYS_linkat 37
+#define SYS_unlinkat 35
+#define SYS_mkdirat 34
+#define SYS_umount2 39
+#define SYS_mount 40
+#define SYS_fstat 80
 
-    // Process Management
-    SYS_fork,
-    SYS_execve,
-    SYS_exit,
-    SYS_wait4,
-    SYS_getpid,
-    SYS_getppid,
-    SYS_kill,
+// Process Management
+#define SYS_clone 220
+#define SYS_execve 221
+#define SYS_wait4 260
+#define SYS_exit 93
+#define SYS_getppid 173
+#define SYS_getpid 172
 
-    // IPC
-    SYS_pipe,
-    SYS_shmget,
-    SYS_shmat,
-    SYS_msgget,
-    SYS_msgsnd,
+// Memory Management
+#define SYS_brk 214
+#define SYS_munmap 215
+#define SYS_mmap 222
 
-    // Memory Management
-    SYS_brk,
-    SYS_mmap,
-    SYS_munmap,
-    SYS_mprotect,
+// Others
+#define SYS_times 153
+#define SYS_uname 160
+#define SYS_sched_yield 124
+#define SYS_gettimeofday 169
+#define SYS_nanosleep 101
 
-    // Time
-    SYS_time,
-    SYS_gettimeofday,
-    SYS_nanosleep,
-
-    // Signals
-    SYS_sigaction,
-    SYS_sigprocmask,
-
-    NR_SYSCALL
-};
 
 typedef uint64 (*syscall_func_t)(void);
 
