05a1335e39479e4f197ec6d7bfb2f43db7d4b0f6
WhaleFall <166845494@qq.com>
2025-05-30 21:24:09 +0800

    info trap detail

--- kernel/arch/loongarch/trap/trap.c
+++ kernel/arch/loongarch/trap/trap.c
@@ -16,6 +16,7 @@ extern void timer_isr();
 handler interrupt_handler_vector[NR_INTERRUPT] = {};
 handler exception_handler_vector[NR_EXCEPTION] = {};
 
+static void log_trap_details(int ecode, int esubcode);
 
 void info_exception() {
     Log(ANSI_FG_RED, "ERA: %lx", r_csr_era());
@@ -23,6 +24,7 @@ void info_exception() {
     int ecode = ((estat & CSR_ESTAT_Ecode) >> 16);
     int esubcode = ((estat & CSR_ESTAT_EsubCode) >> 22);
     Log(ANSI_FG_RED, "Ecode: %d, Esubcode: %d", ecode, esubcode);
+    log_trap_details(ecode, esubcode);
     Log(ANSI_FG_RED, "BADV: %lx", r_csr_badv());
 }
 
@@ -151,3 +153,49 @@ dive_to_user()
     ((void (*)(uint64, uint64))fn)(trapframe, pgdl);
 }
 
+
+
+
+
+typedef struct {
+    unsigned int ecode;          // 异常码
+    unsigned int esubcode;       // 子异常码
+    const char* name;            // 异常名称
+    const char* description;     // 异常说明
+    const char* category;        // 所属类别
+} ExceptionInfo;
+
+const ExceptionInfo exceptions[] = {
+    {0x1, 0, "PIL", "load操作页无效异常", "地址转换异常"},
+    {0x2, 0, "PIS", "store操作页无效异常", "地址转换异常"},
+    {0x3, 0, "PIF", "取指操作页无效异常", "地址转换异常"},
+    {0x4, 0, "PME", "页修改异常", "地址转换异常"},
+    {0x5, 0, "PNR", "页不可读异常", "地址转换异常"},
+    {0x6, 0, "PNX", "页不可执行异常", "地址转换异常"},
+    {0x7, 0, "PPI", "页权限等级不合规异常", "地址转换异常"},
+    {0x8, 0x0, "ADEF", "取指地址错异常", "指令执行中的错误"},
+    {0x8, 0x1, "ADEM", "访存指令地址错异常", "指令执行中的错误"},
+    {0x9, 0, "ALE", "地址非对齐异常", "指令执行中的错误"},
+    {0xA, 0, "BCE", "边界约束检查错异常", "指令执行中的错误"},
+    {0xB, 0, "SYS", "系统调用异常", "系统调用和陷入"},
+    {0xC, 0, "BRK", "断点异常", "系统调用和陷入"},
+    {0xD, 0, "INE", "指令不存在异常", "指令执行中的错误"},
+    {0xE, 0, "IPE", "指令权限等级错异常", "指令执行中的错误"},
+    {0xF, 0, "FPD", "浮点指令未使能异常", "系统调用和陷入"},
+    {0x10, 0, "SXD", "128位向量扩展指令未使能异常", "系统调用和陷入"},
+    {0x11, 0, "ASXD", "256位向量扩展指令未使能异常", "系统调用和陷入"},
+    {0x12, 0x0, "FPE", "基础浮点指令异常", "需要软件修正的运算"},
+    {0x12, 0x1, "VFPE", "向量浮点指令异常", "需要软件修正的运算"},
+    {0x13, 0x0, "WPEF", "取指监测点异常", "系统调用和陷入"},
+    {0x13, 0x1, "WPEM", "load/store操作监测点异常", "系统调用和陷入"},
+};
+
+static void log_trap_details(int ecode, int esubcode) {
+    for (int i = ecode - 1; i < 0x13; i++) {
+        if (exceptions[i].ecode == ecode && exceptions[i].esubcode == esubcode) {
+            Log(ANSI_FG_RED, "Exception: %s (%s)", exceptions[i].name, exceptions[i].description);
+            // Log(ANSI_FG_RED, "Category: %s", exceptions[i].category);
+            return;
+        }
+    }
+}
